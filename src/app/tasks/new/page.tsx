bef0857d93029b642fa079c0297016dda46c829e
e.zheltukhin <e.zheltukhin@kt-team.de>
2025-05-15 18:16:41 +0500

    Add tasks form layout

--- src/app/tasks/new/page.tsx
+++ src/app/tasks/new/page.tsx
@@ -1,3 +1,4 @@
+import Button from "@/shared/ui/Button";
 import Title from "@/shared/ui/Title";
 
 export default function TasksNewPage() {
@@ -6,6 +7,51 @@ export default function TasksNewPage() {
       <div className="flex justify-between items-center">
         <Title>Adding new task</Title>
       </div>
+      <form>
+        <div className="border-b border-gray-900/10 pb-12">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="col-span-full">
+              <label htmlFor="title" className="block text-sm/6 font-medium text-gray-900">
+                Title
+              </label>
+              <div className="mt-2">
+                <input
+                  id="title"
+                  name="title"
+                  type="text"
+                  autoComplete="title"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label htmlFor="description" className="block text-sm/6 font-medium text-gray-900">
+                Description
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="description"
+                  name="description"
+                  rows={3}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  defaultValue={''}
+                />
+              </div>
+              <p className="mt-3 text-sm/6 text-gray-600">Write a few sentences to describe task.</p>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" className="text-sm/6 font-semibold text-gray-900">
+              Cancel
+            </button>
+            <Button type="submit"btn="primary">
+              Save
+            </Button>
+          </div>
+        </div>
+      </form>
     </>
   )
 }
