f66e8b1d0f3eb4f7c66359e71f99db089c84ad07
e.zheltukhin <e.zheltukhin@kt-team.de>
2025-05-19 17:07:26 +0500

    Edit Link styles

--- src/shared/ui/Link/index.tsx
+++ src/shared/ui/Link/index.tsx
@@ -3,10 +3,10 @@ import { tv } from "tailwind-variants";
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'
 
 const linkStyle = tv({
-  base: 'cursor-pointer focus:outline-none focus:ring-1 font-medium text-sm rounded-lg px-2.5 py-1.5',
+  base: 'cursor-pointer focus:outline-none focus:underline font-medium text-sm rounded-lg px-2.5 py-1.5',
   variants: {
     schema: {
-        link: 'font-medium text-blue-600 dark:text-blue-500 hover:text-blue-700',
+        link: 'text-blue-600 dark:text-blue-500 hover:text-blue-700',
         btn: 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
     },
   },
